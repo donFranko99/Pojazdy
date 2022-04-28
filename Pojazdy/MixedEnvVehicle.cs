0395452a08b8177da60231157d92737d6ce61654
donFranko99 <79981376+donFranko99@users.noreply.github.com>
2022-04-28 18:12:14 +0200

    trying to figure out mixed vehicle

--- Pojazdy/MixedEnvVehicle.cs
+++ Pojazdy/MixedEnvVehicle.cs
@@ -7,8 +7,10 @@ namespace Pojazdy
     public class MixedEnvVehicle:BaseVehicle
     {
         private Environment currentEnvironment;
+
         public double MaxAltVelocity { get; }
-        public Environment AlternateEnvironment { get; set; }
+        public double MinAltVelocity { get; }
+        public Environment AlternateEnvironment { get; }
         public void SwitchCurrentEnvironment()
         {
             if (currentEnvironment==Environment)
@@ -17,6 +19,12 @@ namespace Pojazdy
                 return;
             }
             currentEnvironment = Environment;
+        }
+        public MixedEnvVehicle(string name, double minVelocity = 1, double maxVelocity = 40, double minAltVelocity = 1, double maxAltVelocity = 40, Environment environment = Environment.Ground, Environment alternateEnvironment = Environment.Water, Engine engine = null) : base(name, minVelocity, maxVelocity, environment, engine)
+        {
+            MinAltVelocity = minVelocity;
+            MaxAltVelocity = maxVelocity;
+            AlternateEnvironment = alternateEnvironment;
         }
 
     }
