a74a167e1bd40c3f0f67cd863dda461e665522c9
donFranko99 <79981376+donFranko99@users.noreply.github.com>
2022-04-28 15:23:39 +0200

    started to implement classes inheriting from base vehicle

---
+++ Pojazdy/LandVehicle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pojazdy
+{
+    public class LandVehicle : BaseVehicle
+    {
+        public LandVehicle(string name, double minVelocity = 1, double maxVelocity = 350, Environment environment = Environment.Ground, Engine engine = null) : base(name, minVelocity, maxVelocity, environment, engine){}
+    }
+}
---
+++ Pojazdy/WaterVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pojazdy
+{
+    public class WaterVehicle : BaseVehicle
+    {
+        public WaterVehicle(string name, double minVelocity = 1, double maxVelocity = 40, Environment environment = Environment.Water, Engine engine = null) : base(name, minVelocity, maxVelocity, environment, engine)
+        {
+            if (engine!=null)
+            {
+                this.Engine.Type = Engine.EngineType.Diesel;
+            }
+        }
+    }
+}
